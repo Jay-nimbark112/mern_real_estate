d2f4bfd9d204fa43de0070dbb4f8f5b0ecf8fc31
techjay <jaynimbark5869@gmail.com>
2026-03-24 13:40:59 +0530

    add css to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from './pages/Home';
-import signIn from './pages/SignIn';
-import signUp from './pages/SignUp';
+import SignIn from './pages/signIn';
+import SignUp from './pages/signUp';
 import Profile from './pages/Profile';
 import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/sign-in" element={<SignIn/>}/>
